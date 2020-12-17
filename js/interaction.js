69d64e6c472f739f16ee6e3d17bb99055f7b8e19
Michael Deng <yd2473@columbia.edu>
2020-12-17 22:10:00 +0800

    1

--- js/interaction.js
+++ js/interaction.js
@@ -1,9 +1,19 @@
 
-
+var current;
 
 
 player.getCurrentTime().then(function(seconds) {
   current = seconds;
 });
+
+//display loader1 only when video is loaded.
+//When user clicks starting screen, loader2 appears and background turns dark until DisplayPart is loaded
+
+function playDisplayPart() {
+	player.setCurrentTime(30.456);
+	document.getElementById("play").style.display = "none";
+	document.getElementById("loader1").style.display = "none";
+}
+
 
 
